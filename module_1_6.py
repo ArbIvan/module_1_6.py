8b0ec203bc9b31e707722fba065a17f038834c50
ArbIvan <arb1984ivan@gmail.com>
2024-11-19 21:54:41 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework_assignments = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_hours_spent / Number_of_completed_homework_assignments
+print ('Курс:', str(Course_name) + ", ", 'всего задач:', str(Number_of_completed_homework_assignments) + ", ", 'затрачено часов:', str(Number_of_hours_spent) + ", ", 'среднее время выполнения:', str(Time_for_one_task), 'часа')
